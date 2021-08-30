5c7699bcfde66b4b657f277fa51cbc750821bd0a
reget17 <reget17@mail.ru>
2021-08-30 11:47:32 +0300

    Done

--- lesson_002/00_distance.py
+++ lesson_002/00_distance.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+from pprint import pprint
 
 # Есть словарь координат городов
+
 
 sites = {
     'Moscow': (550, 370),
@@ -15,9 +17,27 @@ sites = {
 
 distances = {}
 
-# TODO здесь заполнение словаря
+moscow = sites['Moscow']
+london = sites['London']
+paris = sites['Paris']
 
-print(distances)
+distances['Moscow'] = {}
+moscow_london = ((moscow[0]-london[0]) ** 2 + (moscow[1]-london[1]) ** 2) ** 0.5
+moscow_paris = ((moscow[0]-paris[0]) ** 2 + (moscow[1]-paris[1]) ** 2) ** 0.5
+distances['Moscow']['London'] = moscow_london
+distances['Moscow']['Paris'] = moscow_paris
+
+distances['London'] = {}
+london_paris = ((london[0]-paris[0]) ** 2 + (london[1]-paris[1]) ** 2) ** 0.5
+
+distances['London']['Paris'] = london_paris
+distances['London']['Moscow'] = moscow_london
+
+distances['Paris'] = {}
+distances['Paris']['Moscow'] = moscow_paris
+distances['Paris']['London'] = london_paris
+
+pprint(distances)
 
 
 
