ef665086ba73ab5c48735d9269b23df56222a71a
reget17 <reget17@mail.ru>
2021-09-08 20:12:28 +0300

    Done

--- lesson_005/01_inhabitants.py
+++ lesson_005/01_inhabitants.py
@@ -2,7 +2,14 @@
 
 # Вывести на консоль жителей комнат (модули room_1 и room_2)
 # Формат: В комнате room_1 живут: ...
+from pprint import pprint
 
-# TODO здесь ваш код
+import room_1
+import room_2
+
+print('В комнате room_1 живут:', *room_1.folks)
+print('В комнате room_2 живут:', *room_2.folks)
+
+
 
 
