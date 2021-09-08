4171326b0bec869bbcac7ba6f46813ea95050058
reget17 <reget17@mail.ru>
2021-09-08 20:24:26 +0300

    Done

--- lesson_005/02_district.py
+++ lesson_005/02_district.py
@@ -5,7 +5,28 @@
 # подсказка: для вывода элементов списка через запятую можно использовать функцию строки .join()
 # https://docs.python.org/3/library/stdtypes.html#str.join
 
-# TODO здесь ваш код
+import district.central_street.house1.room1
+import district.central_street.house1.room2
+import district.central_street.house2.room1
+import district.central_street.house2.room2
+
+import district.soviet_street.house1.room1
+import district.soviet_street.house1.room2
+import district.soviet_street.house2.room1
+import district.soviet_street.house2.room2
+
+district_people = []
+district_people.extend(district.central_street.house1.room1.folks)
+district_people.extend(district.central_street.house1.room2.folks)
+district_people.extend(district.central_street.house2.room1.folks)
+district_people.extend(district.central_street.house2.room2.folks)
+district_people.extend(district.soviet_street.house1.room1.folks)
+district_people.extend(district.soviet_street.house1.room2.folks)
+district_people.extend(district.soviet_street.house2.room1.folks)
+district_people.extend(district.soviet_street.house2.room2.folks)
+s = ', '
+
+print(s.join(district_people))
 
 
 
