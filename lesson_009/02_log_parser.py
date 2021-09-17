c496ddd902a501a76b4b2e5485fc287ea06e42cb
reget17 <reget17@mail.ru>
2021-09-17 17:49:15 +0300

    Done

--- lesson_009/02_log_parser.py
+++ lesson_009/02_log_parser.py
@@ -19,10 +19,72 @@
 # Входные параметры: файл для анализа, файл результата
 # Требования к коду: он должен быть готовым к расширению функциональности. Делать сразу на классах.
 
-# TODO здесь ваш код
-
 # После выполнения первого этапа нужно сделать группировку событий
 #  - по часам
 #  - по месяцу
 #  - по году
 # Для этого пригодится шаблон проектирование "Шаблонный метод" см https://goo.gl/Vz4828
+from pprint import pprint
+
+
+class FileAnalysis:
+
+    def __init__(self, input_file, out_file):
+        self.input_file = input_file
+        self.output_file = out_file
+        self.filtered_text = {}
+        self.group_text = {}
+
+
+    def import_file(self):
+        with open(self.input_file, 'r', encoding='cp1251') as file:
+            for line in file:
+                self.filter_text(line)
+
+    def filter_text(self, line):
+        if 'NOK' in line:
+            temp_line = line[:17] + ']'
+            if temp_line in self.filtered_text:
+                self.filtered_text[temp_line] += 1
+            else:
+                self.filtered_text[temp_line] = 1
+
+    def group(self, mode):
+        user_mode = 0
+        self.group_text = {}
+        if mode == 'hour':
+            user_mode = -4
+        elif mode == 'month':
+            user_mode = -10
+        elif mode == 'year':
+            user_mode = 5
+        else:
+            print('Вы ввели некорректный режим для группировки. Введите hour, month или year')
+        for date, count in self.filtered_text.items():
+            temp_date = date[:user_mode] + ']'
+            if temp_date in self.group_text:
+                self.group_text[temp_date] += count
+            else:
+                self.group_text[temp_date] = count
+
+
+    def writing_file(self):
+        with open(self.output_file, 'w', encoding='cp1251') as file:
+            content = ''
+            for date, count in self.group_text.items():
+                content += f'{date} {count}\n'
+            file.write(content)
+            pprint(content)
+
+
+input_file = 'events.txt'
+output_file = 'out.txt'
+file = FileAnalysis(input_file, output_file)
+file.import_file()
+file.group(mode='hour')
+# file.group(mode='month')
+# file.group(mode='year')
+file.writing_file()
+
+
+
