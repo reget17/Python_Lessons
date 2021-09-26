5f69b0764679621d40747ba5ab510037784e9e57
reget17 <reget17@mail.ru>
2021-09-26 21:20:45 +0300

    Done

--- lesson_010/01_fifth_element.py
+++ lesson_010/01_fifth_element.py
@@ -3,11 +3,18 @@
 # Умножить константу BRUCE_WILLIS на пятый элемент строки, введенный пользователем
 
 BRUCE_WILLIS = 42
+try:
+    input_data = input('Если хочешь что-нибудь сделать, сделай это сам: ')
+    leeloo = int(input_data[4])
+    result = BRUCE_WILLIS * leeloo
+    print(f"- Leeloo Dallas! Multi-pass № {result}!")
+except ValueError as exc:
+    print('ValueError: invalid literal for ', exc)
+except IndexError:
+    print('Ошибка IndexError: string index out of range')
+except():
+    print('Другая ошибка')
 
-input_data = input('Если хочешь что-нибудь сделать, сделай это сам: ')
-leeloo = int(input_data[4])
-result = BRUCE_WILLIS * leeloo
-print(f"- Leeloo Dallas! Multi-pass № {result}!")
 
 # Ообернуть код и обработать исключительные ситуации для произвольных входных параметров
 # - ValueError - невозможно преобразовать к числу
