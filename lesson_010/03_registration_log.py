728fd631af7af40e89e03a8396d447e42069f036
reget17 <reget17@mail.ru>
2021-09-27 15:04:51 +0300

    Done

--- lesson_010/03_registration_log.py
+++ lesson_010/03_registration_log.py
@@ -22,4 +22,69 @@
 # - поле возраст НЕ является числом от 10 до 99: ValueError
 # Вызов метода обернуть в try-except.
 
-# TODO здесь ваш код
+
+class NotNameError(Exception):
+    pass
+
+
+class NotEmailError(Exception):
+    pass
+
+
+class RegProtocol:
+
+    def __init__(self, input_file):
+        self.input_file = input_file
+        self.registrations_good = 'registrations_good.log'
+        self.registrations_bad = 'registrations_bad.log'
+
+    def start(self):
+        with open(self.input_file, 'r', encoding='UTF8') as file:
+            for line in file:
+                line = line[:-1]
+                try:
+                    self.validate(line=line)
+                    self.write_good(line)
+                except ValueError as exc:
+                    self.write_bad(line, mistake='ValueError', arg=exc)
+                except NotNameError as exc:
+                    self.write_bad(line, mistake='NotNameError', arg=exc)
+                except NotEmailError as exc:
+                    self.write_bad(line, mistake='NotEmailError', arg=exc)
+
+    def validate(self, line):
+        line_list = line.split(sep=' ')
+
+        if len(line_list) != 3:
+            raise ValueError('Введены не все 3 поля')
+        elif not line_list[0].isalpha():
+            raise NotNameError('В имени не только буквы')
+        elif not ('@' in line_list[1] and '.' in line_list[1]):
+            raise NotEmailError('Ошибка в email')
+        elif not 10 <= int(line_list[2]) <= 99:
+            raise ValueError('Некорректный возраст')
+
+    def write_good(self, text):
+        with open(self.registrations_good, 'a', encoding='UTF8') as file:
+            file.write(text)
+            file.write('\n')
+
+    def write_bad(self, text, mistake, arg=None):
+        with open(self.registrations_bad, 'a', encoding='UTF8') as file:
+            file.write(f'{text} | {mistake} | {arg}')
+            file.write('\n')
+
+    def clean_files(self):
+        with open(self.registrations_bad, 'w') as file:
+            file.write('')
+        with open(self.registrations_good, 'w') as file:
+            file.write('')
+
+
+file = 'registrations.txt'
+myfile = RegProtocol(file)
+myfile.clean_files()
+myfile.start()
+
+
+
